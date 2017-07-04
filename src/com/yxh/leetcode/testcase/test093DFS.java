f1157554ef20ccb8e6cf40898ce27e3d0e04d574
yuxianhan <yuxianhan1@jd.com>
2017-07-04 17:33:25 +0800

    DSADSADSADa

---
+++ src/com/yxh/leetcode/testcase/test093DFS.java
@@ -0,0 +1,39 @@
+package com.yxh.leetcode.testcase;
+
+import java.util.ArrayList;
+
+public class test093DFS {
+	public ArrayList<String> restoreIpAddresses(String s) {  
+        ArrayList<String> res = new ArrayList<String>();  
+        String item = new String();
+        if (s.length()<4||s.length()>12) 
+        return res;  
+        
+        dfs(s, 0, item, res);  
+        return res;  
+    }  
+      
+    public void dfs(String s, int start, String item, ArrayList<String> res){  
+        if (start == 3 && isValid(s)) {  
+            res.add(item + s);  
+            return;  
+        }  
+        for(int i=0; i<3 && i<s.length()-1; i++){  
+            String substr = s.substring(0,i+1);  //某个IP字段     从一个字符开始到3个字符结束
+            if (isValid(substr))
+                dfs(s.substring(i+1, s.length()), start+1, item + substr + '.', res);  
+        }  
+    }  
+      
+    public boolean isValid(String s){  
+        if (s.charAt(0)=='0')
+            return s.equals("0");  
+            
+        int num = Integer.parseInt(s);
+            
+        if(num <= 255 && num > 0)
+            return true;
+        else
+            return false;
+    }
+}
